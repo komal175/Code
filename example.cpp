916dc40ccf3d42220c18d9a962ac400337b61909
komal175 <komal17500@gmail.com>
2024-06-05 19:12:14 +0530

    first commit

---
+++ example.cpp
@@ -0,0 +1,16 @@
+/*#include<bits/stdc++.h> 
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    cout<<++a <<endl;
+    cout<<"sad life";
+}*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    cout<<a*5 <<'\n';
+    cout<<"sad life\n";
+}
