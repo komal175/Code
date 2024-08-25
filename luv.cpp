76162934bc217a23e81eead4d8d1d3dc3a4e5882
komal175 <komal17500@gmail.com>
2024-08-25 12:33:33 +0530

    luv

---
+++ luv.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+     int a,b;
+     cin >>a >>b;
+     for(int i=a;i<=b;i++){
+         if (i==1){
+             cout << "one" <<endl;
+         }
+         else if (i==2){
+             cout << "two"<<endl;
+         }
+         else if (i==3){
+             cout << "three"<<endl;
+         }
+         else if (i==4){
+             cout << "four"<<endl;
+         }
+         else if (i==5){
+             cout << "five"<<endl;
+         }
+         else if (i==6){
+             cout << "six"<<endl;
+         }
+         else if (i==7){
+             cout <<"seven"<<endl;
+         }
+         else if (i==8){
+             cout <<"eight"<<endl;
+         }
+         else if (i==9){
+             cout << "nine"<<endl;
+         }
+         for (int i=a;i>9;i++){
+             if (i%2 ==0){
+         }
+         else{
+             cout <<"odd";
+         }
+         }
+         
+     }
+     return 0;
+ }
+ 
